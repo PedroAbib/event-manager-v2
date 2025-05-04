a9d0879df920637733f0015b3961a28e113ace7a
pedro <paulo-ppaa@hotmail.com>
2025-05-04 09:56:19 -0300

    style: table header

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -21,7 +21,7 @@ function TableHeader({ className, ...props }: React.ComponentProps<"thead">) {
   return (
     <thead
       data-slot="table-header"
-      className={cn("[&_tr]:border-b", className)}
+      className={cn("[&_tr]:border-b dark:bg-[#181818] dark:text-white bg-gray-100", className)}
       {...props}
     />
   )
